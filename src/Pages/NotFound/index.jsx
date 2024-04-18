09df052bf8a81d4b4b484874a89ab1d38866a971
AmirHosein-Erfanian <141142089+AmirHosein-Erfanian@users.noreply.github.com>
2024-04-18 13:25:30 +0430

    "Add Gift Vouchers page with basic layout and text"
    This commit adds a new page for Gift Vouchers with a basic layout and some text. The layout includes a stack with a gray horizontal rule and a stack containing

--- src/Pages/NotFound/index.jsx
+++ src/Pages/NotFound/index.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
-
+import { Stack, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 const NotFound = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Stack sx={{
+                width: '100%',
+                height: 'auto',
+                display: 'flex',
+                backgroundColor: "#F3F3F3",
+                justifyContent: 'center',
+                padding: { lg: '30px 12%', md: "30px 12%", sm: '30px 5%', xs: '30px 2%' }
+            }}>
+                <Stack sx={{
+                    padding: { lg: '20px 17%', md: '20px', sm: '20px', xs: '20px' },
+                    color: 'black',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}>
+
+                    <Typography variant="h5" sx={{ padding: '0px 0px 20px 0px' }}>
+                        404 Page Not Found
+                    </Typography>
+
+                    <Typography variant="body1" >The page you requested does not exist. Click <Link to={'/'} style={{ color: '#931817' }} > here </Link>to continue shopping.</Typography><br />
+
+                </Stack>
+
+            </Stack>
+        </>
     );
 }
 
--- src/Pages/Shippingpolicy/index.jsx
+++ src/Pages/Shippingpolicy/index.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
-
+import { Stack, Typography } from '@mui/material';
 const Shippingpolicy = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Stack sx={{
+                width: '100%',
+                height: 'auto',
+                display: 'flex',
+                backgroundColor: "#F3F3F3",
+                padding: { lg: '30px 12%', md: "30px 12%", sm: '30px 5%', xs: '30px 2%' }
+            }}>
+                <hr color='gray' style={{ clear: 'both', height: '0', borderTop: 'solid #dedede', borderWidth: '1px 0 0', margin: '30px 0 ' }} />
+                <Stack sx={{
+                    padding: { lg: '20px 17%', md: '20px', sm: '20px', xs: '20px' },
+                    color: 'black',
+                    alignItems: 'center'
+                }}>
+
+                    <Typography variant="h5" sx={{ padding: '0px 0px 20px 0px' }}>
+                        Shipping policy
+                    </Typography>
+
+                    <Stack
+                        sx={{
+                            width: '100%',
+                            height: 'auto',
+                        }}>
+                        <Typography variant="body1" > We offer shipping anywhere in Ireland for €3.95 and free shipping over €20. We currently only ship within Ireland, but the whole island of Ireland.</Typography><br />
+                        <Typography variant="body1" > We use An Post Tracked postage and we aim to process orders within 2 working days.</Typography><br />
+                        <Typography variant="body1" > If you haven't received your order within 15 days please contact us.</Typography><br />
+
+                    </Stack>
+
+                </Stack>
+
+            </Stack>
+        </>
     );
 }
 
